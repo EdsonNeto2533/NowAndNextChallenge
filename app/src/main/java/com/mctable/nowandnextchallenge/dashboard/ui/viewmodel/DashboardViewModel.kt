6ae3083718bf82a3a8f94d7086f9fc3c8bc3f59e
Edson <edsonneto2533@gmail.com>
2022-06-24 15:43:32 -0300

    adjusted navigation to DashboardFragment

--- app/src/main/java/com/mctable/nowandnextchallenge/dashboard/ui/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/mctable/nowandnextchallenge/dashboard/ui/viewmodel/DashboardViewModel.kt
@@ -2,8 +2,9 @@ package com.mctable.nowandnextchallenge.dashboard.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
