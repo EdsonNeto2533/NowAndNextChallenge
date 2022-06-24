9336d467dd68da091b5e3bc5e0edc9d80d11581e
Edson <edsonneto2533@gmail.com>
2022-06-24 15:07:49 -0300

    create default application class and added permision to internet

---
+++ app/src/main/java/com/mctable/nowandnextchallenge/commons/di/NowAndNextApp.kt
@@ -0,0 +1,8 @@
+package com.mctable.nowandnextchallenge.commons.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NowAndNextApp : Application() {
+}
