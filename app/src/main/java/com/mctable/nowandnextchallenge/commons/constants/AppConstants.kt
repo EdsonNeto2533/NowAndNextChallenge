3620342e7b19eed374e22096400e0f412a445e30
Edson <edsonneto2533@gmail.com>
2022-06-24 23:32:16 -0300

    get channels list endpoint created

--- app/src/main/java/com/mctable/nowandnextchallenge/commons/constants/AppConstants.kt
+++ app/src/main/java/com/mctable/nowandnextchallenge/commons/constants/AppConstants.kt
@@ -3,4 +3,9 @@ package com.mctable.nowandnextchallenge.commons.constants
 object AppConstants {
 
     const val BASE_URL = "http://ott.online.meo.pt/"
+    const val DEFAULT_USER_AGENT = "AND"
+    const val DEFAULT_FILTER =
+        "substringof%28%27MEO_Mobile%27%2CAvailableOnChannels%29%20and%20IsAdult%20eq%20false"
+    const val DEFAULT_ORDINATION = "ChannelPosition%20asc"
+    const val DEFAULT_INLINE_COUNT = "allpages"
 }
--- app/src/main/java/com/mctable/nowandnextchallenge/dashboard/data/api/ChannelsWebService.kt
+++ app/src/main/java/com/mctable/nowandnextchallenge/dashboard/data/api/ChannelsWebService.kt
@@ -1,5 +1,19 @@
 package com.mctable.nowandnextchallenge.dashboard.data.api
 
+import com.mctable.nowandnextchallenge.commons.constants.AppConstants
+import com.mctable.nowandnextchallenge.dashboard.data.response.ChannelsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ChannelsWebService {
 
+    @GET("catalog/v9/Channels")
+    fun getChannelsList(
+        @Query("UserAgent") userAgent: String = AppConstants.DEFAULT_USER_AGENT,
+        @Query("filter") filter: String = AppConstants.DEFAULT_FILTER,
+        @Query("orderby") orderBy: String = AppConstants.DEFAULT_ORDINATION,
+        @Query("inlinecount") inlineCount: String = AppConstants.DEFAULT_INLINE_COUNT,
+        @Query("skip") skip: String?
+    ): Response<ChannelsResponse>
 }
